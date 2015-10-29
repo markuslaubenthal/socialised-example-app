9723da95db5d5a539fb4765414cb9b9f6c5ec2ec
Markus Laubenthal <mlaubenthalmail@gmail.com>
2015-10-29 23:55:47 +0100

    added PDO Singleton

---
+++ classes/pdo_connection.php
@@ -0,0 +1,48 @@
+<?php
+
+ class Demo {
+   /**
+    * instance
+    *
+    * Statische Variable, um die aktuelle (einzige!) Instanz dieser Klasse zu halten
+    *
+    * @var Singleton
+    */
+   protected static $_instance = null;
+
+   protected $connection;
+
+   /**
+    * get instance
+    *
+    * Falls die einzige Instanz noch nicht existiert, erstelle sie
+    * Gebe die einzige Instanz dann zurück
+    *
+    * @return   Singleton
+    */
+   public static function getInstance()
+   {
+       if (null === self::$_instance)
+       {
+           self::$_instance = new self;
+       }
+       return self::$_instance->connection;
+   }
+
+   /**
+    * clone
+    *
+    * Kopieren der Instanz von aussen ebenfalls verbieten
+    */
+   protected function __clone() {}
+
+     $this->connection = new PDO(MYSQL_HOST, MYSQL_USER, MYSQL_PASS);
+   /**
+    * constructor
+    *
+    * externe Instanzierung verbieten
+    */
+   protected function __construct() {}
+ }
+
+?>
